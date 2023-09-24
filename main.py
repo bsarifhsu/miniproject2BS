a447c3c5b6e67f0623a2fd47d0d972d6a6b87730
bsarifhsu <b_sari@mail.fhsu.edu>
2023-09-24 18:46:34 -0500

    Functions added in order to tide up the code.

--- main.py
+++ main.py
@@ -6,47 +6,45 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-
-# Dataset is downloaded from : https://www.kaggle.com/datasets/mexwell/us-school-scores
-# Read the data from DataSet school_scores.csv and save it to Pandas DataFrame
-schoolScores = pd.read_csv("school_scores.csv",index_col="Year")
-
-# It shows a quick statistic summary of the dataset.
-stats = schoolScores.describe()
-print(stats)
-
-grouped = schoolScores.groupby("State.Code")["Total.Test-takers"].sum()
-
-# Generate a bar chart
-# Sets the size of the figure.
-plt.figure(figsize=(10, 5))
-# Type of the figure is set to Bar.
-plt.bar(grouped.index, grouped.values)
-chartTitle = plt.title("Total Number of Test Takers by State")
-plt.xlabel("States")
-plt.ylabel("Total Test Takers")
-# Turns the States Codes 90 degree upwards.
-plt.xticks(rotation=90)
-plt.tight_layout()
+def readDataSet():
+    # Dataset is downloaded from : https://www.kaggle.com/datasets/mexwell/us-school-scores
+    # Read the data from DataSet school_scores.csv and save it to Pandas DataFrame
+    schoolScores = pd.read_csv("school_scores.csv",index_col="Year")
 
 
-# Plot the Total Number of Test Takers across the United States
-# schoolScores.plot(x="State.Code", y="Total.Test-takers")
-# chartTitle = plt.title("Total Test Takers Across the United States")
-# plt.ylabel("Test Takers")
-# plt.xlabel("States")
+    # It shows a quick statistic summary of the dataset.
+    stats = schoolScores.describe()
+    print(stats)
+    return schoolScores
+
+def generateChart():
+    grouped = pd.DataFrame(readDataSet()).groupby("State.Code")["Total.Test-takers"].sum()
+
+    # Generate a bar chart
+    # Sets the size of the figure.
+    plt.figure(figsize=(10, 5))
+    # Type of the figure is set to Bar.
+    plt.bar(grouped.index, grouped.values)
+    chartTitle = plt.title("Total Number of Test Takers by State")
+    plt.xlabel("States")
+    plt.ylabel("Total Test Takers")
+    # Turns the States Codes 90 degree upwards.
+    plt.xticks(rotation=90)
+    # Axes on the figure adjusted to avoid the overlaying content.
+    plt.tight_layout()
 
 
-# Saves plot
-saveChart = "charts/" + chartTitle.get_text() + ".png"
-plt.savefig(saveChart)
+    # Saves plot
+    saveChart = "charts/" + chartTitle.get_text() + ".png"
+    plt.savefig(saveChart)
 
-# Displays the Chart
-plt.show()
+    # Displays the Chart
+    plt.show()
 
 
 # Start of the program
-
+readDataSet()
+generateChart()
 # It creates the chart folder
 try:
     # Create the charts folder
