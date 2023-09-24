55a4db110466ef2d70e313fa60a602feae6527e9
bsarifhsu <b_sari@mail.fhsu.edu>
2023-09-24 14:30:33 -0500

    Dataset downloaded from Kaggle added.

--- main.py
+++ main.py
@@ -2,17 +2,13 @@
 # Bunyamin Sari
 # Mini Project 2
 
-
-
 # (5/5 points) Proper import of packages used.
 
 import pandas as pd
 
-# (20/20 points) Using a data source of your choice, such as data from data.gov or using the Faker package, generate or retrieve some data for creating basic statistics on. This will generally come in as json data, etc.
-# Think of some question you would like to solve such as:
-# "How many homes in the US have access to 100Mbps Internet or more?"
-# "How many movies that Ridley Scott directed is on Netflix?" - https://www.kaggle.com/datasets/shivamb/netflix-shows
-# Here are some other great datasets: https://www.kaggle.com/datasets
+# Dataset is downloaded from : https://www.kaggle.com/datasets/mexwell/us-school-scores
+
+
 # (10/10 points) Store this information in Pandas dataframe. These should be 2D data as a dataframe, meaning the data is labeled tabular data.
 # (10/10 points) Using matplotlib, graph this data in a way that will visually represent the data. Really try to build some fancy charts here as it will greatly help you in future homework assignments and in the final project.
 # (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
